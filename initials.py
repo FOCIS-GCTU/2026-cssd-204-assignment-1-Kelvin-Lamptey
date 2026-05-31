728753a447dcc8f4be7c1566f750dc77208a061c
Kelvin Nii Odartey Lamptey <78008166+Kelvin-Lamptey@users.noreply.github.com>
2026-05-31 15:52:33 +0000

    implement initials printing function in initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints out my initials.
+# Assignment Number: 1
+#
+# Name: Kelvin Nii Odartey Lamptey
+# STUDENT ID:  2425402471
+# Email: 2425402471@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Kelvin Nii Odartey Lamptey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the initials KNL in a 60-character wide format.
+    print()
+    print("...KNL")
+    print()
+    print("...KK......KK..........NN........NN........LL...............")
+    print("...KK....KK............NNNN......NN........LL...............")
+    print("...KK..KK..............NN.NN.....NN........LL...............")
+    print("...KKKK................NN..NN....NN........LL...............")
+    print("...KK..................NN...NN...NN........LL...............")
+    print("...KKKK................NN....NN..NN........LL...............")
+    print("...KK..KK..............NN.....NN.NN........LL...............")
+    print("...KK....KK............NN......NNNN........LL...............")
+    print("...KK......KK.....**...NN........NN...**...LLLLLLLLLLLL...**")
+    print("...KK........KK...**...NN........NN...**...LLLLLLLLLLLL...**")
+    print()
 
 
 main()
